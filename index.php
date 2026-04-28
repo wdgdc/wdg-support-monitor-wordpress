fa1dd404407b789a47541759b463f23cc5cd875f
Jerry Price <jerry@wdg.co>
2026-04-28 15:15:22 -0400

    use placeholders for building action

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 /**
+ * %s
+ * 
  * Plugin Name: WDG Support Monitor
- * Version: 1.1.1
+ * Version: %s
  * Description: Monitors site status for support.
  * Author: WDG - The Web Development Group
  * Author URI: https://www.webdevelopmentgroup.com
